55a2c8e47fb5472e77892ace648033fb0c83564a
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-07-12 20:05:36 +0530

    add: add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,14 @@ import Nav from './_components/nav';
 
 export default function Home() {
   return (
-    <>
+    <div className='home-container'>
       <Nav />
-    </>
+      <div className='flex justify-end md:mt-[-85px]'>
+          <img className='w-[80%] z-[-1]' src='homebg.png' />
+      </div>
+      <div className='flex justify-end'>
+        <img className='w-[50%] h-[50%] mt-[-180px] md:mt-[-750px]' src='food.png' />
+      </div>
+    </div>
   );
-}
+} 
