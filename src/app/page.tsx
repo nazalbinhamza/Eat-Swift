e0e9fd387f60391435ffeaae12482e22451097eb
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-07-13 11:07:54 +0530

    add: add headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,12 @@ export default function Home() {
       <div className='flex justify-end'>
         <img className='w-[50%] h-[50%] mt-[-180px] md:mt-[-750px]' src='food.png' />
       </div>
+      <div className='flex justify-start'>
+        <h1 className='text-[20px] font-serif ml-[20px] mt-[-170px] md:ml-[70px] md:mt-[-620px] md:text-[70px]'>Enjoy Healthy And<br></br>Delicious Food</h1>
+      </div>
+      <div className='flex justify-start'>
+        <button className=' bg-black text-white w-[120px] h-[30px] ml-[20px] mt-[-100px] md:w-[200px] md:h-[70px] rounded-full md:mt-[-400px] md:ml-[70px]'>Order Now</button>
+      </div>
     </div>
   );
 } 
